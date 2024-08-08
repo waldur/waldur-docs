91e037c9dfcbeaec470d12a6531012870cbce0f3
ci <ci@waldur.com>
2024-08-08 11:17:32 +0000

    Deployed 91a80f1 with MkDocs version: 1.6.0

---
+++ developer-guide/scripts/import_projects_with_slug.py
@@ -0,0 +1,37 @@
+"""
+This script reads CSV-file containing projects info
+and creates/updates the corresponding projects in Waldur.
+"""
+
+#!/bin/python
+
+import csv
+import datetime
+
+from waldur_core.structure import models as structure_models
+
+
+def import_project(project_info):
+    project_id = project_info["id"]
+    valid_from = datetime.datetime.strptime(project_info["valid_from"], "%d/%m/%Y").date()
+    valid_to = datetime.datetime.strptime(project_info["valid_to"], "%d/%m/%Y").date()
+    description = project_info.get("description", "")
+    project, created = structure_models.Project.objects.update_or_create(
+        name=project_id,
+        defaults={
+            "created": valid_from,
+            "description": description,
+            "end_date": valid_to,
+            "slug": project_id,
+        },
+    )
+    if created:
+        print(f"The project {project.name} has been created")
+    else:
+        print(f"The project {project.name} has been updated")
+
+
+with open("/tmp/projects-info.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for project_data in reader:
+        create_project(project_data)
